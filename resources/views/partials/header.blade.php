bc3df889418e00ad8fd410634fe1439ef8bef22b
nhat191024 <richberchannel01@gmail.com>
2024-09-26 13:02:39 +0700

    Refactor: code format

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,167 +1,172 @@
 <header class="main-header header-style-one">
 
-            <div class="header-top">
-                <div class="top-left">
+    <div class="header-top">
+        <div class="top-left">
 
-                    <ul class="list-style-one">
-                        <li><i class="fa fa-map-marker-alt"></i> {{__('P.114, T.1, Thành Đạt 1, số 3 Lê Thánh Tông, P.Máy Tơ, Q.Ngô Quyền, HP')}}</li>
-                        <li><i class="fa fa-clock"></i> Thứ 2 - Thứ 7: 8am - 5pm</li>
-                        <li><i class="fa fa-phone-volume"></i> <a href="tel:+84968633588">+84 968633588</a></li>
-                    </ul>
-                </div>
-                <div class="top-right">
-                    <ul class="social-icon-one">
-                        <li>
-                            <a href="#"><span class="fab fa-facebook-square"></span></a>
-                        </li>
-                        <li>
-                            <a href="#"><span class="fab fa-twitter"></span></a>
-                        </li>
-                        <li>
-                            <a href="#"><span class="fab fa-pinterest-p"></span></a>
-                        </li>
-                        <li>
-                            <a href="#"><span class="fab fa-instagram"></span></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            <ul class="list-style-one">
+                <li><i class="fa fa-map-marker-alt"></i>
+                    {{ __('P.114, T.1, Thành Đạt 1, số 3 Lê Thánh Tông, P.Máy Tơ, Q.Ngô Quyền, HP') }}</li>
+                <li><i class="fa fa-clock"></i> Thứ 2 - Thứ 7: 8am - 5pm</li>
+                <li><i class="fa fa-phone-volume"></i> <a href="tel:+84968633588">+84 968633588</a></li>
+            </ul>
+        </div>
+        <div class="top-right">
+            <ul class="social-icon-one">
+                <li>
+                    <a href="#"><span class="fab fa-facebook-square"></span></a>
+                </li>
+                <li>
+                    <a href="#"><span class="fab fa-twitter"></span></a>
+                </li>
+                <li>
+                    <a href="#"><span class="fab fa-pinterest-p"></span></a>
+                </li>
+                <li>
+                    <a href="#"><span class="fab fa-instagram"></span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
 
 
-            <div class="header-lower">
+    <div class="header-lower">
 
-                <div class="main-box">
-                    <div class="logo-box">
-                        <div class="logo">
-                            <a href="{{route('home')}}"><img src="{{asset('huaxia/images/logo.jpg')}}" alt title="Huaxia" /></a>
-                        </div>
-                    </div>
-
-                    <div class="nav-outer">
-                        <nav class="nav main-menu">
-                            <ul class="navigation">
-                                <li class="current dropdown">
-                                    <a href="{{route('home')}}">{{__('Trang chủ')}}</a>
-                                    
-                                </li>
-                                <li class="dropdown">
-                                    <a href="#">{{__('Giới thiệu')}}</a>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="#">{{__('Dịch vụ')}}</a>
-                                </li>
-                                <li><a href="{{route('contact')}}">{{__('Liên hệ')}}</a></li>
-                            </ul>
-                        </nav>
-
-                        <div class="outer-box">
-                            <button class="ui-btn ui-btn search-btn">
-                                <span class="icon lnr lnr-icon-search"></span>
-                            </button>
-                           <!-- <a href="#" class="ui-btn"><i class="lnr-icon-shopping-cart"></i></a> -->
-                            <a href="#" class="theme-btn btn-style-one alternate"><span class="btn-title">{{__('Đặt dịch vụ')}}</span></a>
-
-                            <div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
-                        </div>
-                    </div>
+        <div class="main-box">
+            <div class="logo-box">
+                <div class="logo">
+                    <a href="{{ route('home') }}"><img src="{{ asset('huaxia/images/logo.jpg') }}" alt
+                            title="Huaxia" /></a>
                 </div>
             </div>
 
+            <div class="nav-outer">
+                <nav class="nav main-menu">
+                    <ul class="navigation">
+                        <li class="current dropdown">
+                            <a href="{{ route('home') }}">{{ __('Trang chủ') }}</a>
 
-            <div class="mobile-menu">
-                <div class="menu-backdrop"></div>
-
-                <nav class="menu-box">
-                    <div class="upper-box">
-                        <div class="nav-logo">
-                            <a href="{{route('home')}}"><img src="{{asset('huaxia/images/logo-navi.jpg')}}" alt title="Huaxia" /></a>
-                        </div>
-                        <div class="close-btn"><i class="icon fa fa-times"></i></div>
-                    </div>
-                    <ul class="navigation clearfix">
-
-                    </ul>
-                    <ul class="contact-list-one">
-                        <li>
-
-                            <div class="contact-info-box">
-                                <i class="icon lnr-icon-phone-handset"></i>
-                                <span class="title">{{__('Gọi Ngay')}}</span>
-                                <a href="tel:+84968633588">0968633588</a>
-                            </div>
                         </li>
-                        <li>
-
-                            <div class="contact-info-box">
-                                <span class="icon lnr-icon-envelope1"></span>
-                                <span class="title">{{__('Gửi email')}}</span>
-                                <a href=""><span>info@huaxiavietnam.com</span></a>
-                            </div>
+                        <li class="dropdown">
+                            <a href="{{ route('about') }}">{{ __('Giới thiệu') }}</a>
                         </li>
-                        <li>
-
-                            <div class="contact-info-box">
-                                <span class="icon lnr-icon-clock"></span>
-                                <span class="title">{{__('Gửi email')}}</span>
-                                Mon - Sat 8:00 - 6:30, Sunday - CLOSED
-                            </div>
+                        <li class="dropdown">
+                            <a href="#">{{ __('Dịch vụ') }}</a>
                         </li>
-                    </ul>
-                    <ul class="social-links">
-                        <li>
-                            <a href="#"><i class="fab fa-twitter"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fab fa-pinterest"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                        </li>
+                        <li><a href="{{ route('contact') }}">{{ __('Liên hệ') }}</a></li>
                     </ul>
                 </nav>
-            </div>
 
+                <div class="outer-box">
+                    <button class="ui-btn ui-btn search-btn">
+                        <span class="icon lnr lnr-icon-search"></span>
+                    </button>
+                    <!-- <a href="#" class="ui-btn"><i class="lnr-icon-shopping-cart"></i></a> -->
+                    <a href="#" class="theme-btn btn-style-one alternate"><span
+                            class="btn-title">{{ __('Đặt dịch vụ') }}</span></a>
 
-            <div class="search-popup">
-                <span class="search-back-drop"></span>
-                <button class="close-search"><span class="fa fa-times"></span></button>
-                <div class="search-inner">
-                    <form method="post" action="{{route('home')}}">
-                        <div class="form-group">
-                            <input type="search" name="search-field" value placeholder="{{__('Tìm kiếm...')}}" required />
-                            <button type="submit"><i class="fa fa-search"></i></button>
-                        </div>
-                    </form>
+                    <div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
                 </div>
             </div>
+        </div>
+    </div>
 
 
-            <div class="sticky-header">
-                <div class="auto-container">
-                    <div class="inner-container">
+    <div class="mobile-menu">
+        <div class="menu-backdrop"></div>
 
-                        <div class="logo">
-                            <a href="{{route('home')}}" title><img src="{{asset('huaxia/images/logo-navi.png')}}" alt title /></a>
-                        </div>
+        <nav class="menu-box">
+            <div class="upper-box">
+                <div class="nav-logo">
+                    <a href="{{ route('home') }}"><img src="{{ asset('huaxia/images/logo-navi.jpg') }}" alt
+                            title="Huaxia" /></a>
+                </div>
+                <div class="close-btn"><i class="icon fa fa-times"></i></div>
+            </div>
+            <ul class="navigation clearfix">
 
-                        <div class="nav-outer">
+            </ul>
+            <ul class="contact-list-one">
+                <li>
 
-                            <nav class="main-menu">
-                                <div class="navbar-collapse show collapse clearfix">
-                                    <ul class="navigation clearfix">
-
-                                    </ul>
-                                </div>
-                            </nav>
-
-
-                            <div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
-                        </div>
+                    <div class="contact-info-box">
+                        <i class="icon lnr-icon-phone-handset"></i>
+                        <span class="title">{{ __('Gọi Ngay') }}</span>
+                        <a href="tel:+84968633588">0968633588</a>
                     </div>
+                </li>
+                <li>
+
+                    <div class="contact-info-box">
+                        <span class="icon lnr-icon-envelope1"></span>
+                        <span class="title">{{ __('Gửi email') }}</span>
+                        <a href=""><span>info@huaxiavietnam.com</span></a>
+                    </div>
+                </li>
+                <li>
+
+                    <div class="contact-info-box">
+                        <span class="icon lnr-icon-clock"></span>
+                        <span class="title">{{ __('Gửi email') }}</span>
+                        Mon - Sat 8:00 - 6:30, Sunday - CLOSED
+                    </div>
+                </li>
+            </ul>
+            <ul class="social-links">
+                <li>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-pinterest"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+
+    <div class="search-popup">
+        <span class="search-back-drop"></span>
+        <button class="close-search"><span class="fa fa-times"></span></button>
+        <div class="search-inner">
+            <form method="post" action="{{ route('home') }}">
+                <div class="form-group">
+                    <input type="search" name="search-field" value placeholder="{{ __('Tìm kiếm...') }}" required />
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    <div class="sticky-header">
+        <div class="auto-container">
+            <div class="inner-container">
+
+                <div class="logo">
+                    <a href="{{ route('home') }}" title><img src="{{ asset('huaxia/images/logo-navi.png') }}" alt
+                            title /></a>
+                </div>
+
+                <div class="nav-outer">
+
+                    <nav class="main-menu">
+                        <div class="navbar-collapse show collapse clearfix">
+                            <ul class="navigation clearfix">
+
+                            </ul>
+                        </div>
+                    </nav>
+
+
+                    <div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
                 </div>
             </div>
+        </div>
+    </div>
 
-        </header>
+</header>
