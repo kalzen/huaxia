7e4c8d89a71dd98f46293c3f64ba68ef409a31c2
nhat191024 <richberchannel01@gmail.com>
2024-09-27 20:42:55 +0700

    Refactor: add nav lang file

---
+++ lang/en/nav.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    "address" => "P.114, T.1, Thanh Dat 1, No. 3 Le Thanh Tong, May To Ward, Ngo Quyen District, HP",
+    "work_time" => "Monday - Saturday",
+    "language" => "Language",
+    "lang" => [
+        "en" => "English",
+        "vi" => "Vietnamese"
+    ],
+    "home" => "Home",
+    "about" => "About",
+    "services" => "Services",
+    "post" => "Post",
+    "contact" => "Contact",
+    "book_service" => "Book service",
+    "call_callout" => "CALL NOW",
+    "email_callout" => "SEND EMAIL",
+    "work_time_callout" => "WORKING HOURS",
+    "work_time_mobile" => "Monday - Saturday 8:00 - 6:30, Sunday - CLOSED",
+];
+    
