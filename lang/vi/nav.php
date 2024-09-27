343f4cbe5bc51bb68098507fddbbc5d75e1592cf
nhat191024 <richberchannel01@gmail.com>
2024-09-27 20:42:39 +0700

    Refactor: change lang file location

---
+++ lang/vi/nav.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    "address" => "P.114, T.1, Thành Đạt 1, số 3 Lê Thánh Tông, P.Máy Tơ, Q.Ngô Quyền, HP",
+    "work_time" => "Thứ 2 - Thứ 7",
+    "language" => "Ngôn ngữ",
+    "lang" => [
+        "en" => "Tiếng Anh",
+        "vi" => "Tiếng Việt"
+    ],
+    "home" => "Trang chủ",
+    "about" => "Giới thiệu",
+    "services" => "Dịch vụ",
+    "post" => "Tin tức",
+    "contact" => "Liên hệ",
+    "book_service" => "Đặt dịch vụ",
+    "call_callout" => "GỌI NGAY",
+    "email_callout" => "GỬI EMAIL",
+    "work_time_callout" => "GIỜ LÀM VIỆC",
+    "work_time_mobile" => "Thứ 2 - Thứ 7 8:00 - 6:30, Chủ Nhật - CLOSED",
+];
