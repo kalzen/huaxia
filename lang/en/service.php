72f6401f8f7fca097e0eaf0f4c634c4634a21d15
nhat191024 <richberchannel01@gmail.com>
2024-09-28 13:47:40 +0700

    Refactor: Add language files for service page translations in English and Vietnamese

---
+++ lang/en/service.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    "page_title" => "About",
+    "page_title_breadcrumb_1" => "Home",
+    "page_title_breadcrumb_2" => "Service",
+
+    "service_box_subtitle_1" => "Service 1",
+    "service_box_title_1" => "Air transport",
+    "service_box_subtitle_2" => "Service 2",
+    "service_box_title_2" => "Sea transport",
+    "service_box_subtitle_3" => "Service 3",
+    "service_box_title_3" => "Road transport",
+    "service_box_subtitle_4" => "Service 4",
+    "service_box_title_4" => "Other solutions",
+];
---
+++ lang/vi/service.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    "page_title" => "Dịch vụ",
+    "page_title_breadcrumb_1" => "Trang chủ",
+    "page_title_breadcrumb_2" => "Dịch vụ",
+
+    "service_box_subtitle_1" => "Dịch vụ 1",
+    "service_box_title_1" => "Vận chuyển <br /> hàng không",
+    "service_box_subtitle_2" => "Dịch vụ 2",
+    "service_box_title_2" => "Vận chuyển <br /> đường biển",
+    "service_box_subtitle_3" => "Dịch vụ 3",
+    "service_box_title_3" => "Vận chuyển <br /> đường bộ",
+    "service_box_subtitle_4" => "Dịch vụ 4",
+    "service_box_title_4" => "Giải pháp <br /> khác",
+];
