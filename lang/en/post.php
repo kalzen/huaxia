0ebca1529585bad599f9f1d555fcc78d860a3429
nhat191024 <richberchannel01@gmail.com>
2024-09-28 13:47:25 +0700

    Refactor: Add language file for post page in English and Vietnamese translations

---
+++ lang/en/post.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    "page_title" => "Post",
+    "page_title_breadcrumb_1" => "Home",
+    "page_title_breadcrumb_2" => "Post",
+];
---
+++ lang/vi/post.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    "page_title" => "Dịch vụ",
+    "page_title_breadcrumb_1" => "Trang chủ",
+    "page_title_breadcrumb_2" => "Dịch vụ",
+];
