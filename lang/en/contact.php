ea4870d2faa3e0cadb9e41fb74b5427d09ca4227
nhat191024 <richberchannel01@gmail.com>
2024-09-28 13:47:09 +0700

    Refactor: Update language file references in contact.php for English and Vietnamese translations

---
+++ lang/en/contact.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    "page_title" => "Contact",
+    "page_title_breadcrumb_1" => "Home",
+    "page_title_breadcrumb_2" => "Contact",
+
+    "subtitle_1" => "Need help?",
+    "subtitle_2" => "Contact us",
+    "subtitle_3" => "Send email to us",
+    "subtitle_4" => "Feel free to write",
+
+    "box_title_1" => "Have a question?",
+    "box_subtitle_1" => "Free",
+    "box_title_2" => "Send Email",
+    "box_title_3" => "Visit anytime",
+    "box_subtitle_3" => "66 broklyn golden street. New York",
+
+    "form_label_1" => "Name",
+    "form_label_2" => "Email",
+    "form_label_3" => "Subject",
+    "form_label_4" => "Phone",
+    "form_label_5" => "Message",
+
+    "form_placeholder_1" => "Enter your name",
+    "form_placeholder_2" => "Enter your email",
+    "form_placeholder_3" => "Enter subject",
+    "form_placeholder_4" => "Enter phone",
+    "form_placeholder_5" => "Enter message",
+
+    "form_button" => "Send message",
+    "form_reset" => "Reset",
+];
---
+++ lang/vi/contact.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    "page_title" => "Liên hệ",
+    "page_title_breadcrumb_1" => "Trang chủ",
+    "page_title_breadcrumb_2" => "Liên hệ",
+
+    "subtitle_1" => "Cần giúp đỡ?",
+    "subtitle_2" => "Liên hệ với chúng tôi",
+    "subtitle_3" => "Gửi email cho chúng tôi",
+    "subtitle_4" => "Hãy viết thoải mái",
+
+    "box_title_1" => "Có câu hỏi?",
+    "box_subtitle_1" => "Miễn phí",
+    "box_title_2" => "Gửi Email",
+    "box_title_3" => "Thăm bất cứ lúc nào",
+    "box_subtitle_3" => "66 broklyn golden street. New York",
+
+    "form_label_1" => "Tên",
+    "form_label_2" => "Email",
+    "form_label_3" => "Tiêu đề",
+    "form_label_4" => "Số điên thoại",
+    "form_label_5" => "Tin nhắn",
+
+    "form_placeholder_1" => "Nhập tên của bạn",
+    "form_placeholder_2" => "Nhập email của bạn",
+    "form_placeholder_3" => "Nhập tiêu đề",
+    "form_placeholder_4" => "Nhập số điện thoại",
+    "form_placeholder_5" => "Nhập tin nhắn",
+
+    "form_button" => "Gửi tin nhắn",
+    "form_reset" => "Đặt lại",
+];
