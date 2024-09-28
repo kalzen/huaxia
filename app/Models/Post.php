6f73412b7d6e2e0686259c38ed15bd7eac92bf36
nhat191024 <richberchannel01@gmail.com>
2024-09-28 18:29:51 +0700

    Refactor: format

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,44 +14,51 @@ class Post extends Model
     protected $guarded = [];
     public function getUrlAttribute()
     {
-        return '/tin-tuc/'.$this->slug;
+        return '/tin-tuc/' . $this->slug;
     }
-    public function scopeActive($query) {
+    public function scopeActive($query)
+    {
         $query->where('status', Post::STATUS_ACTIVE);
     }
-    public function scopeIsPromotion($query) {
+
+    public function scopeIsPromotion($query)
+    {
         $query->where('is_promotion', Post::STATUS_ACTIVE);
     }
+
     public function comments()
     {
         return $this->morphMany(Comment::class, 'commentable');
     }
+
     public function tags()
     {
         return $this->morphToMany(Tag::class, 'taggable')->withTimestamps();
     }
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
     public function images()
     {
         return $this->belongsToMany(Image::class)->withTimestamps();
     }
+
     public function categories()
     {
         return $this->belongsToMany(Category::class)->withTimestamps();
     }
+    
     public static function boot()
     {
         parent::boot();
-        static::creating(function($post)
-        {
+        static::creating(function ($post) {
             $post->slug = $post->slug ?: (Str::slug($post->title));
             $post->user_id = $post->user_id ?: (auth()->user()->id ?? null);
         });
-        static::updating(function($post)
-        {
+        static::updating(function ($post) {
             $post->slug = Str::slug($post->title);
         });
     }
