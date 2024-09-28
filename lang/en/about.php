46d942f4221ba0daa2e87e54bf907ffa08dab382
nhat191024 <richberchannel01@gmail.com>
2024-09-28 13:47:20 +0700

    Refactor: Update language file references in about.php for English and Vietnamese translations

---
+++ lang/en/about.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    "page_title" => "About",
+    "page_title_breadcrumb_1" => "Home",
+    "page_title_breadcrumb_2" => "About",
+
+    "about_title" => "We provide comprehensive </br> shipping services",
+    "about_description" => "Huaxia Vietnam Supply Chain Management Co., Ltd. always strives to become a delivery company that customers entrust with high trust and trust, supporting the connection of Vietnamese businesses to the world with high-quality services, professional, dedicated personnel, creating added value, contributing positively to the development of the supply chain",
+    "about_box_title_1" => "International Freight Forwarding",
+    "about_box_description_1" => "We always provide everyone with a complete solution focusing on any business",
+    "about_box_title_2" => "Trade promotion",
+    "about_box_description_2" => "We always provide everyone with a complete solution focusing on any business",
+    "about_button" => "Read more",
+    "about_experience_year" => "Year",
+    "about_experience" => "EXPERIENCE",
+
+    "service_subtitle" => "SPECIALIZING IN TRANSPORTATION",
+    "service_title" => "Professional logistics services",
+    "service_box_subtitle_1" => "Service 1",
+    "service_box_title_1" => "Air transport",
+    "service_box_subtitle_2" => "Service 2",
+    "service_box_title_2" => "Sea transport",
+    "service_box_subtitle_3" => "Service 3",
+    "service_box_title_3" => "Road transport",
+    "service_box_subtitle_4" => "Service 4",
+    "service_box_title_4" => "Other solutions",
+];
---
+++ lang/vi/about.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    "page_title" => "Giới thiệu",
+    "page_title_breadcrumb_1" => "Trang chủ",
+    "page_title_breadcrumb_2" => "Giới thiệu",
+
+    "about_title" => "Chúng tôi cung cấp các dịch vụ vận chuyển </br> trọn gói",
+    "about_description" => "Công ty TNHH Quản lý chuỗi cung ứng Huaxia Việt Nam luôn nỗ lực phấn đấu trở thành công ty giao nhận hàng được khách hàng gửi gắm niềm tiên và sự tín nhiệm cao, hỗ trợ kết nối các doanh nghiệp Việt Nam vươn ra thế giới với dịch vụ chat lượng cao, nhân sự chuyên nghiệp, tận tâm, tạo ra giá trị gia tăng, đóng góp tích cực vào sự phát triển của chuỗi cung ứng",
+    "about_box_title_1" => "Vận chuyển hàng hoá quốc tế",
+    "about_box_description_1" => "Chúng tôi luôn cung cấp cho mọi người một giải pháp hoàn chỉnh tập trung vào bất kỳ doanh nghiệp nào",
+    "about_box_title_2" => "Xúc tiến thương mại",
+    "about_box_description_2" => "Chúng tôi luôn cung cấp cho mọi người một giải pháp hoàn chỉnh tập trung vào bất kỳ doanh nghiệp nào",
+    "about_button" => "Xem thêm",
+    "about_experience_year" => "Năm",
+    "about_experience" => "KINH NGHIỆM",
+
+    "service_subtitle" => "CHUYÊN VỀ VẬN TẢI",
+    "service_title" => "Dịch vụ logistics chuyên nghiệp",
+    "service_box_subtitle_1" => "Dịch vụ 1",
+    "service_box_title_1" => "Vận chuyển <br /> hàng không",
+    "service_box_subtitle_2" => "Dịch vụ 2",
+    "service_box_title_2" => "Vận chuyển <br /> đường biển",
+    "service_box_subtitle_3" => "Dịch vụ 3",
+    "service_box_title_3" => "Vận chuyển <br /> đường bộ",
+    "service_box_subtitle_4" => "Dịch vụ 4",
+    "service_box_title_4" => "Giải pháp <br /> khác",
+];
