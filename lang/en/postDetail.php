dff74bf3bf403358f20779301e630d2cbb3b8d84
nhat191024 <richberchannel01@gmail.com>
2024-09-30 00:15:24 +0700

    Refactor: Add language support for post detail page

---
+++ lang/en/postDetail.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    "page_title" => "Post",
+    "page_title_breadcrumb_1" => "Home",
+    "page_title_breadcrumb_2" => "Post",
+
+    "creator" => "Admin",
+    "tag" => "Tags",
+    "search_placeholder" => "Search here...",
+    "latest_post" => "Latest Posts",
+    "category" => "Categories",
+
+];
---
+++ lang/vi/postDetail.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    "page_title" => "Tin tức",
+    "page_title_breadcrumb_1" => "Trang chủ",
+    "page_title_breadcrumb_2" => "Tin tức",
+
+
+    "creator" => "Quản trị viên",
+    "tag" => "Thẻ",
+    "search_placeholder" => "Tìm kiếm...",
+    "latest_post" => "Bài viết mới nhất",
+    "category" => "Danh mục",
+];
