5559127525ff07d6248ee3116aae4f2053ab7e14
nhat191024 <richberchannel01@gmail.com>
2024-09-30 21:54:51 +0700

    Feat: Add LanguageHelper.php for converting language codes to full language names

---
+++ app/Helpers/LanguageHelper.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!function_exists('convertLanguageCode')) {
+    /**
+     * Convert language code to full language name.
+     *
+     * @param string $code
+     * @return string
+     */
+    function convertLanguageCode($code)
+    {
+        $languages = [
+            'en' => 'Tiếng Anh',
+            'vi' => 'Tiếng Việt',
+        ];
+
+        return $languages[$code] ?? $code;
+    }
+}
