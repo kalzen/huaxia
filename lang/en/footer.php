9833157410b7e59e83d6630b33e4e2f8b7309ce3
nhat191024 <richberchannel01@gmail.com>
2024-10-10 01:08:33 +0700

    Refactor: Add footer translations for English and Vietnamese

---
+++ lang/en/footer.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    "address" => "Address",
+    "address_content" => "30 St Kilda Road, Jackson Store, Australia",
+
+    "contact" => "Contact",
+
+    "time" => "Working time",
+    "time_content" => "Monday - Friday: 8:00 - 22:00",
+
+    "about" => "About us",
+
+    "sub_title_1" => "We work with passion to face challenges and create new things in the advertising field",
+
+    "service" => "Service",
+    "service_content_1" => "Reliable & On time",
+    "service_content_2" => "Reliable franchise",
+    "service_content_3" => "Storage warehouse",
+    "service_content_4" => "Real-time tracking",
+    "service_content_5" => "Transparent price",
+
+    "news" => "News",
+    "news_content" => "The latest news, advice & best articles from the blog",
+    "news_form_placeholder" => "Your email",
+
+    "copy_right" => "All rights reserved",
+];
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -11,8 +11,8 @@
                         <div class="contact-info-block col-lg-4 col-md-6 col-sm-12 wow fadeInRight">
                             <div class="inner-box">
                                 <div class="icon-box"><i class="icon flaticon-international-shipping-2"></i></div>
-                                <h4 class="title">Address</h4>
-                                <div class="text">30 St Kilda Road, Jackson Store, Australia</div>
+                                <h4 class="title">{{ __('footer.address') }}</h4>
+                                <div class="text">{{ __('footer.address_content') }}</div>
                             </div>
                         </div>
 
@@ -20,7 +20,7 @@
                             data-wow-delay="300ms">
                             <div class="inner-box">
                                 <div class="icon-box"><i class="icon flaticon-stock-1"></i></div>
-                                <h4 class="title">Contact</h4>
+                                <h4 class="title">{{ __('footer.contact') }}</h4>
                                 <div class="text">
                                     <a
                                         href="https://html.kodesolution.com/cdn-cgi/l/email-protection#57393232333f323b271734383a2736392e7934383a"><span
@@ -35,8 +35,8 @@
                             data-wow-delay="600ms">
                             <div class="inner-box">
                                 <div class="icon-box"><i class="icon flaticon-24-hours-2"></i></div>
-                                <h4 class="title">Timing</h4>
-                                <div class="text">Mon - Sat: 8 am - 5 pm, Sunday: CLOSED</div>
+                                <h4 class="title">{{ __('footer.time') }}</h4>
+                                <div class="text">{{ __('footer.time_content') }}</div>
                             </div>
                         </div>
                     </div>
@@ -53,64 +53,38 @@
                                 <div class="logo">
                                     <a href="{{route('home')}}"><img src="{{asset('huaxia/images/logo-2.png')}}" alt /></a>
                                 </div>
-                                <div class="text">We work with a passion of taking challenges and creating new ones in
-                                    advertising sector.</div>
+                                <div class="text">{{ __('footer.sub_title_1') }}</div>
                                 <a href="page-about.html" class="theme-btn btn-style-one hvr-light small"><span
-                                        class="btn-title">About</span></a>
+                                        class="btn-title">{{ __('footer.about') }}</span></a>
                             </div>
                         </div>
 
                         <div class="footer-column col-xl-3 col-lg-3 col-md-6 col-sm-12">
                             <div class="footer-widget">
-                                <h3 class="widget-title">Service</h3>
+                                <h3 class="widget-title">{{ __('footer.service') }}</h3>
                                 <ul class="user-links">
-                                    <li><a href="#">Reliability & Punctuality</a></li>
-                                    <li><a href="#">Trusted Franchise</a></li>
-                                    <li><a href="#">Warehoues Storage</a></li>
-                                    <li><a href="#">Real Time Tracking</a></li>
-                                    <li><a href="#">Transparent Pricing</a></li>
+                                    <li><a href="#">{{ __('footer.service_content_1') }}</a></li>
+                                    <li><a href="#">{{ __('footer.service_content_2') }}</a></li>
+                                    <li><a href="#">{{ __('footer.service_content_3') }}</a></li>
+                                    <li><a href="#">{{ __('footer.service_content_4') }}</a></li>
+                                    <li><a href="#">{{ __('footer.service_content_5') }}</a></li>
                                 </ul>
                             </div>
                         </div>
 
                         <div class="footer-column col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                            <div class="footer-widget gallery-widget">
-                                <h3 class="widget-title">Projects</h3>
-                                <div class="widget-content">
-                                    <div class="outer clearfix">
-                                        <figure class="image">
-                                            <a href="#"><img src="{{asset('huaxia/images/resource/project-thumb-1.jpg')}}" alt /></a>
-                                        </figure>
-                                        <figure class="image">
-                                            <a href="#"><img src="{{asset('huaxia/images/resource/project-thumb-2.jpg')}}" alt /></a>
-                                        </figure>
-                                        <figure class="image">
-                                            <a href="#"><img src="{{asset('huaxia/images/resource/project-thumb-3.jpg')}}" alt /></a>
-                                        </figure>
-                                        <figure class="image">
-                                            <a href="#"><img src="{{asset('huaxia/images/resource/project-thumb-4.jpg')}}" alt /></a>
-                                        </figure>
-                                        <figure class="image">
-                                            <a href="#"><img src="{{asset('huaxia/images/resource/project-thumb-5.jpg')}}" alt /></a>
-                                        </figure>
-                                        <figure class="image">
-                                            <a href="#"><img src="{{asset('huaxia/images/resource/project-thumb-6.jpg')}}" alt /></a>
-                                        </figure>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
 
                         <div class="footer-column col-xl-3 col-lg-5 col-md-6 col-sm-12">
                             <div class="footer-widget">
-                                <h3 class="widget-title">Newsletter</h3>
+                                <h3 class="widget-title">{{ __('footer.news') }}</h3>
                                 <div class="widget-content">
                                     <div class="subscribe-form">
-                                        <div class="text">Subscribe our newsletter to get our latest update & news</div>
+                                        <div class="text">{{ __('footer.news_content') }}</div>
                                         <form method="post" action="#">
                                             <div class="form-group">
                                                 <input type="email" name="email" class="email" value
-                                                    placeholder="Email Address" required />
+                                                    placeholder="{{ __('footer.news_form_placeholder') }}" required />
                                                 <button type="button" class="theme-btn btn-style-one">
                                                     <span class="btn-title"><i class="fa fa-paper-plane"></i></span>
                                                 </button>
@@ -128,7 +102,7 @@
                 <div class="auto-container">
                     <div class="inner-container">
                         <div class="copyright-text">
-                            <p>&copy; Copyright Reserved by <a href="{{route('home')}}">kodesolution.com</a></p>
+                            <p>&copy; {{ __('footer.copy_right') }} <a href="{{route('home')}}">kodesolution.com</a></p>
                         </div>
                         <ul class="social-icon-two">
                             <li>
