e1cb601b4b9f44515f3de74048034ff74cc2f200
nhat191024 <richberchannel01@gmail.com>
2024-10-14 02:33:42 +0700

    Refactor: Update vision and mission page translations in English and Vietnamese

---
+++ lang/en/vision&mission.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    "page_title" => "Vision & Mission",
+    "page_title_breadcrumb_1" => "Home",
+    "page_title_breadcrumb_2" => "Vision & Mission",
+
+    "vision&mission" => "Our Vision & Mission",
+    "vision_subtitle" => "Vision",
+    "vision_content" => "Huaxia Vietnam Supply Chain Management Co., Ltd. always strives to become a company that customers entrust with the highest trust and trust in the market. We are committed to providing high-quality services, with dedication and professionalism from the human resources team, always focusing on the smallest details in the process of serving customers. Huaxia is not only a bridge between Vietnamese businesses and the international market, but also aims to create added value, enhance benefits for customers and partners. Thereby, we hope to make an important contribution to promoting sustainable and comprehensive development of the supply chain, while raising the position of Vietnamese businesses in the process of global economic integration. The development of Huaxia is not only limited to building trust with customers, but also aims to contribute to the development of the country's economy, with the ambition to become a leading enterprise in the logistics industry.",
+
+    "mission_subtitle" => "Mission",
+    "mission_content" => "Huaxia Vietnam Supply Chain Management Co., Ltd. understands that maintaining and developing high-quality transportation services, ensuring safety, accuracy and speed is the most important and important task in all activities. We always put the interests of customers first, ensuring that each shipment is not only on time but also complies with strict standards of quality and safety. Huaxia is committed to bringing customers optimal transportation solutions, suitable for the specific needs of each customer group, thereby helping them optimize costs and time. Currently, our company provides a variety of logistics services, serving many different fields, including items from machinery, oversized, overweight, to agricultural products, consumer goods, supplies, chemicals and many other types of goods, fully meeting all transportation needs of the market today.",
+];
---
+++ lang/vi/vision&mission.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    "page_title" => "Tầm nhìn & Sứ mệnh",
+    "page_title_breadcrumb_1" => "Trang chủ",
+    "page_title_breadcrumb_2" => "Tầm nhìn & Sứ mệnh",
+
+    "vision&mission" => "Tầm nhìn & Sứ mệnh của chúng tôi",
+    "vision_subtitle" => "Tầm nhìn",
+    "vision_content" => "Công ty TNHH Quản lý chuỗi cung ứng Huaxia Việt Nam luôn không ngừng nỗ lực để phấn đấu trở thành công ty
+                giao nhận hàng hóa được khách hàng gửi gắm niềm tin và sự tín nhiệm cao nhất trên thị trường. Chúng tôi cam
+                kết cung cấp các dịch vụ chất lượng cao, với sự tận tâm và chuyên nghiệp từ đội ngũ nhân sự, luôn chú trọng
+                đến từng chi tiết nhỏ nhất trong quá trình phục vụ khách hàng. Huaxia không chỉ đơn thuần là cầu nối giữa
+                các doanh nghiệp Việt Nam và thị trường quốc tế mà còn hướng tới mục tiêu tạo ra giá trị gia tăng, nâng cao
+                lợi ích cho khách hàng và đối tác. Qua đó, chúng tôi mong muốn góp phần quan trọng vào việc thúc đẩy sự phát
+                triển bền vững và toàn diện của chuỗi cung ứng, đồng thời nâng tầm vị thế của các doanh nghiệp Việt Nam
+                trong quá trình hội nhập kinh tế toàn cầu. Sự phát triển của Huaxia không chỉ dừng lại ở việc xây dựng niềm
+                tin với khách hàng mà còn mong muốn đóng góp vào sự phát triển của nền kinh tế đất nước, với khát vọng trở
+                thành một trong những doanh nghiệp đầu ngành trong lĩnh vực logistics.",
+
+    "mission_subtitle" => "Sứ mệnh",
+    "mission_content" => "Công ty TNHH Quản lý chuỗi cung ứng Huaxia Việt Nam hiểu rõ rằng việc duy trì và phát triển các dịch vụ vận
+                tải chất lượng cao, đảm bảo an toàn, chính xác và nhanh chóng là nhiệm vụ xuyên suốt và quan trọng nhất
+                trong mọi hoạt động. Chúng tôi luôn đặt lợi ích của khách hàng lên hàng đầu, đảm bảo rằng mỗi chuyến hàng
+                được giao nhận không chỉ đúng thời gian mà còn tuân thủ các tiêu chuẩn khắt khe về chất lượng và an toàn.
+                Huaxia cam kết mang đến cho khách hàng những giải pháp vận chuyển tối ưu, phù hợp với từng nhu cầu riêng
+                biệt của từng đối tượng khách hàng, từ đó giúp họ tối ưu hóa chi phí và thời gian. Hiện nay, công ty chúng
+                tôi cung cấp dịch vụ logistics đa dạng, phục vụ cho nhiều lĩnh vực khác nhau, bao gồm các mặt hàng từ máy
+                móc, hàng siêu trường, siêu trọng, đến nông sản, hàng tiêu dùng, vật tư, hóa chất và nhiều loại hàng hóa
+                khác, đáp ứng đầy đủ mọi nhu cầu vận chuyển của thị trường hiện nay.",
+];
