3b073a12adcd334aaaa679433089668c1eb2b257
nhat191024 <richberchannel01@gmail.com>
2024-10-14 02:33:38 +0700

    Refactor: Update 'post' navigation label to 'News'

--- lang/en/post.php
+++ lang/en/post.php
@@ -1,6 +1,8 @@
 <?php
 return [
-    "page_title" => "Post",
+    "page_title" => "News",
     "page_title_breadcrumb_1" => "Home",
-    "page_title_breadcrumb_2" => "Post",
+    "page_title_breadcrumb_2" => "News",
+
+    "no_post" => "No post to display",
 ];
--- lang/vi/post.php
+++ lang/vi/post.php
@@ -3,4 +3,6 @@ return [
     "page_title" => "Tin tức",
     "page_title_breadcrumb_1" => "Trang chủ",
     "page_title_breadcrumb_2" => "Tin tức",
+
+    "no_post" => "Không có bài viết nào",
 ];
