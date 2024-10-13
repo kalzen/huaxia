c10b287c9bfeb08f328c46899d5ffe981682a652
nhat191024 <richberchannel01@gmail.com>
2024-10-14 02:34:13 +0700

    Refactor: Update service page layout and content, and optimize service block rendering

--- resources/views/home/service.blade.php
+++ resources/views/home/service.blade.php
@@ -1,80 +1,51 @@
 @extends('layouts.master')
 @section('content')
-<section class="page-title" style="background-image: url({{ asset('huaxia/images/background/2.jpg') }})">
-    <div class="auto-container">
-        <div class="title-outer">
-            <h1 class="title">{{ __('service.page_title') }}</h1>
-            <ul class="page-breadcrumb">
-                <li><a href="{{ route('home') }}">{{ __('service.page_title_breadcrumb_1') }}</a></li>
-                <li>{{ __('service.page_title_breadcrumb_2') }}</li>
-            </ul>
-        </div>
-    </div>
-</section>
-
-    <section class="services-section">
+    <section class="page-title" style="background-image: url({{ asset('huaxia/images/background/2.jpg') }})">
         <div class="auto-container">
+            <div class="title-outer">
+                <h1 class="title">{{ __('service.page_title') }}</h1>
+                <ul class="page-breadcrumb">
+                    <li><a href="{{ route('home') }}">{{ __('service.page_title_breadcrumb_1') }}</a></li>
+                    <li>{{ __('service.page_title_breadcrumb_2') }}</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="services-section mt-5">
+        <div class="bg-image" style="background-image: url({{ asset('huaxia/images/background/1.jpg') }})"></div>
+        <div class="anim-icons">
+            <span class="icon icon-wave-line"></span>
+        </div>
+        <div class="auto-container">
+            <div class="sec-title text-center">
+                <span class="sub-title">{{ __('home.service_company') }}</span>
+                <h2>{{ __('home.service_title') }}</h2>
+                <h3>{{ __('home.service_sub_title') }}</h3>
+            </div>
             <div class="row">
-
-                <div class="service-block col-xl-3 col-lg-4 col-md-6 col-sm-12 wow fadeInUp">
-                    <div class="inner-box ">
-                        <div class="image-box">
-                            <figure class="image"><a href="page-service-details.html"><img
-                                        src="{{ asset('huaxia/images/resource/service-1.jpg') }}" alt></a></figure>
-                        </div>
-                        <div class="content-box">
-                            <i class="icon flaticon-airplane-2"></i>
-                            <span class="sub-title">{{ __('service.service_box_subtitle_1') }}</span>
-                            <h4 class="title"><a href="page-service-details.html">{!! __('service.service_box_title_1') !!}</a></h4>
-                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="service-block col-xl-3 col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="300ms">
-                    <div class="inner-box ">
-                        <div class="image-box">
-                            <figure class="image"><a href="page-service-details.html"><img
-                                        src="{{ asset('huaxia/images/resource/service-2.jpg') }}" alt></a></figure>
-                        </div>
-                        <div class="content-box">
-                            <i class="icon flaticon-cargo-boat"></i>
-                            <span class="sub-title">{{ __('service.service_box_subtitle_2') }}</span>
-                            <h4 class="title"><a href="page-service-details.html">{!! __('service.service_box_title_2') !!}</a></h4>
-                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing</div>
+                @foreach ($services as $service)
+                    <div class="service-block col-xl-3 col-lg-4 col-md-6 col-sm-12 wow fadeInUp">
+                        <div class="inner-box">
+                            <div class="image-box">
+                                <figure class="image">
+                                    <a href="{{ route('post.detail', ['alias' => $service->slug]) }}"><img
+                                            src="{{ asset($service->images->first()->url) }}" alt /></a>
+                                </figure>
+                            </div>
+                            <div class="content-box">
+                                <span class="sub-title"></span>
+                                <h4 class="title">
+                                    <a
+                                        href="{{ route('post.detail', ['alias' => $service->slug]) }}">{{ $service->title }}</a>
+                                </h4>
+                                <div class="text">
+                                    {{ $service->description }}
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="service-block col-xl-3 col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="600ms">
-                    <div class="inner-box ">
-                        <div class="image-box">
-                            <figure class="image"><a href="page-service-details.html"><img
-                                        src="{{ asset('huaxia/images/resource/service-3.jpg') }}" alt></a></figure>
-                        </div>
-                        <div class="content-box">
-                            <i class="icon flaticon-delivery-truck-3"></i>
-                            <span class="sub-title">{{ __('service.service_box_subtitle_3') }}</span>
-                            <h4 class="title"><a href="page-service-details.html">{!! __('service.service_box_title_3') !!}</a></h4>
-                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="service-block col-xl-3 col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="900ms">
-                    <div class="inner-box ">
-                        <div class="image-box">
-                            <figure class="image"><a href="page-service-details.html"><img
-                                        src="{{ asset('huaxia/images/resource/service-4.jpg') }}" alt></a></figure>
-                        </div>
-                        <div class="content-box">
-                            <i class="icon flaticon-delivery-box-4"></i>
-                            <span class="sub-title">{{ __('service.service_box_subtitle_4') }}</span>
-                            <h4 class="title"><a href="page-service-details.html">{!! __('service.service_box_title_4') !!}</a></h4>
-                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing</div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </section>
